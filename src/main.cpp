668dc0a8f4aa0565aa275833bb891977815d60ad
Godet Léandre <leandre.godet@epitech.eu>
2025-11-04 15:06:55 +0100

    add open file and display file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
-#include "main.hpp"
-int main(int argc, const char** argv)
+#include "window.hpp"
+#include <QApplication>
+
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    QApplication app(argc, argv);
+    
+    // Configuration de l'application
+    QApplication::setApplicationName("EpiGimp 2.0");
+    QApplication::setApplicationVersion("1.0");
+    QApplication::setOrganizationName("Epitech");
+    
+    MainWindow window;
+    window.show();
+    
+    return app.exec();
 }
