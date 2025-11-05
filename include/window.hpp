cd660db732f9ec4227e6b8e29c254cf18deedc59
Godet Léandre <leandre.godet@epitech.eu>
2025-11-05 15:01:50 +0100

    add separation in multiple file

--- include/window.hpp
+++ include/window.hpp
@@ -1,68 +1,56 @@
-#ifndef MAINWINDOW_HPP
-#define MAINWINDOW_HPP
+#pragma once
 
 #include <QAction>
-#include <QBuffer>
-#include <QDataStream>
-#include <QFile>
 #include <QImage>
 #include <QLabel>
 #include <QMainWindow>
 #include <QMenu>
-#include <QPixmap>
 #include <QScrollArea>
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+    friend class ImageActions;
 
    public:
     explicit MainWindow(QWidget* parent = nullptr);
-    ~MainWindow() override;
+    ~MainWindow();
 
    private slots:
-    void newImage();
-    void openImage();
-    void saveImage();
-    void saveAsEpg();
-    void openEpg();
-    void closeImage();
     void zoomIn();
     void zoomOut();
     void resetZoom();
+
+    void openEpg();
+    void saveAsEpg();
 
    private:
     void createActions();
     void createMenus();
     void updateImageDisplay();
     void scaleImage(double factor);
-    bool openEpgFormat(const QString& fileName);
-    bool saveEpgFormat(const QString& fileName);
 
-    // Widgets
+    bool saveEpgFormat(const QString& fileName);
+    bool openEpgFormat(const QString& fileName);
+
+    // Membres internes
     QLabel* m_imageLabel;
     QScrollArea* m_scrollArea;
-
-    // Données
     QImage m_currentImage;
     QString m_currentFileName;
     double m_scaleFactor;
 
-    // Menus
     QMenu* m_fileMenu;
     QMenu* m_viewMenu;
 
-    // Actions
     QAction* m_newAct;
     QAction* m_openAct;
-    QAction* m_openEpgAct;
     QAction* m_saveAct;
-    QAction* m_saveEpgAct;
     QAction* m_closeAct;
     QAction* m_exitAct;
     QAction* m_zoomInAct;
     QAction* m_zoomOutAct;
     QAction* m_resetZoomAct;
+    QAction* m_openEpgAct;
+    QAction* m_saveEpgAct;
 };
-
-#endif  // MAINWINDOW_HPP
