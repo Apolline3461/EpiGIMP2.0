03f4b90f8e71c3c235b2223cb590ce7da940858a
Léandre <91626869+Leandre17@users.noreply.github.com>
2025-11-17 15:48:23 +0100

    Feat/handlefile #5 (#25)
    merge branch feat/handlefile

---
+++ src/epgformat.cpp
@@ -0,0 +1,65 @@
+#include "epgformat.hpp"
+
+#include <QBuffer>
+#include <QDataStream>
+#include <QFile>
+
+namespace EpgFormat
+{
+bool save(const QString& fileName, const QImage& image)
+{
+    QFile file(fileName);
+    if (!file.open(QIODevice::WriteOnly))
+        return false;
+
+    QDataStream out(&file);
+    out.setVersion(QDataStream::Qt_6_0);
+
+    out << QString("EPIGIMP");
+    out << qint32(1);
+    out << image.width();
+    out << image.height();
+    out << qint32(image.format());
+
+    QByteArray imageData;
+    QBuffer buffer(&imageData);
+    buffer.open(QIODevice::WriteOnly);
+    image.save(&buffer, "PNG");
+
+    out << imageData;
+    return true;
+}
+
+bool load(const QString& fileName, QImage& outImage)
+{
+    QFile file(fileName);
+    if (!file.open(QIODevice::ReadOnly))
+        return false;
+
+    QDataStream in(&file);
+    in.setVersion(QDataStream::Qt_6_0);
+
+    QString header;
+    in >> header;
+    if (header != "EPIGIMP")
+        return false;
+
+    qint32 version;
+    in >> version;
+    if (version != 1)
+        return false;
+
+    int width, height;
+    qint32 format;
+    in >> width >> height >> format;
+
+    QByteArray imageData;
+    in >> imageData;
+
+    QBuffer buffer(&imageData);
+    buffer.open(QIODevice::ReadOnly);
+    outImage.load(&buffer, "PNG");
+
+    return !outImage.isNull();
+}
+}  // namespace EpgFormat
