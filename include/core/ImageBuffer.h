562c7ac0ec6f89ca79d25e3a8b5f30241c50c7c9
apolline <apolline.fontaine@epitech.eu>
2025-11-24 15:36:13 +0100

    ADD : ImageBuffer skeleton and failing unit tests - Add ImageBuffer header and stub implementation (width/height/strideBytes, data, fill, getPixel, setPixel) - Add GoogleTest suite for ImageBuffer:   - HasCorrectDimensions   - SetAndgetPixelOnCorners   - FillAllPixels   - DataPointerIsNotNullAndConsistent - Code compiles, but tests are expected to fail for now except FillAllPixels.

--- include/core/ImageBuffer.h
+++ include/core/ImageBuffer.h
@@ -2,11 +2,32 @@
 // Created by apolline on 17/11/2025.
 //
 
-#ifndef EPIGIMP2_0_IMAGEBUFFER_H
-#define EPIGIMP2_0_IMAGEBUFFER_H
+#pragma once
+
+#include <cstdint>
+#include <vector>
 
 class ImageBuffer
 {
+public:
+  ImageBuffer(int width, int height);
+
+  int width() const noexcept;
+  int height() const noexcept;
+  int strideBytes() const noexcept;
+
+  uint8_t* dawta() noexcept;
+  const uint8_t* data() const noexcept;
+
+  void fill(uint32_t rgba);
+  uint32_t getPixel(int x, int y) const;
+  void setPixel(int x, int y, uint32_t rgba);
+
+private:
+  int width_{};
+  int height_{};
+  int stride_{};
+
+  std::vector<uint8_t> rgbaPixels_;
 };
 
-#endif  // EPIGIMP2_0_IMAGEBUFFER_H
--- src/core/ImageBuffer.cpp
+++ src/core/ImageBuffer.cpp
@@ -3,3 +3,36 @@
 //
 
 #include "../../include/core/ImageBuffer.h"
+ImageBuffer::ImageBuffer(int width, int height) : width_(width), height_(height)
+{
+    stride_ = width_ * 4;
+}
+
+int ImageBuffer::width() const noexcept
+{
+    return 0;
+}
+
+int ImageBuffer::height() const noexcept
+{
+    return 0;
+}
+
+int ImageBuffer::strideBytes() const noexcept
+{
+    return 0;
+}
+uint8_t* ImageBuffer::dawta() noexcept
+{
+    return nullptr;
+}
+const uint8_t* ImageBuffer::data() const noexcept
+{
+    return nullptr;
+}
+void ImageBuffer::fill(uint32_t rgba) {}
+uint32_t ImageBuffer::getPixel(int x, int y) const
+{
+    return 0;
+}
+void ImageBuffer::setPixel(int x, int y, uint32_t rgba) {}
