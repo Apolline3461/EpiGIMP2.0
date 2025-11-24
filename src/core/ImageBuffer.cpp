5923160ecabf04eb01bcb5688f0f0c5f1fac384b
apolline <apolline.fontaine@epitech.eu>
2025-11-24 15:36:13 +0100

    ADD : ImageBuffer skeleton and failing unit tests - Add ImageBuffer header and stub implementation (width/height/strideBytes, data, fill, getPixel, setPixel) - Add GoogleTest suite for ImageBuffer:   - HasCorrectDimensions   - SetAndgetPixelOnCorners   - FillAllPixels   - DataPointerIsNotNullAndConsistent - Code compiles, but tests are expected to fail for now except HasCorrectDimensions.

--- src/core/ImageBuffer.cpp
+++ src/core/ImageBuffer.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "../../include/core/ImageBuffer.h"
+ImageBuffer::ImageBuffer(const int width, const int height) : width_(width), height_(height)
+{
+    stride_ = width_ * 4;
+}
+
+int ImageBuffer::width() const noexcept
+{
+    return width_;
+}
+
+int ImageBuffer::height() const noexcept
+{
+    return height_;
+}
+
+int ImageBuffer::strideBytes() const noexcept
+{
+    return stride_;
+}
+
+uint8_t* ImageBuffer::data() noexcept
+{
+    return rgbaPixels_.data();
+}
+const uint8_t* ImageBuffer::data() const noexcept
+{
+    return rgbaPixels_.data();
+}
+void ImageBuffer::fill(uint32_t rgba) {}
+uint32_t ImageBuffer::getPixel(int x, int y) const
+{
+    return 0;
+}
+void ImageBuffer::setPixel(int x, int y, uint32_t rgba) {}
