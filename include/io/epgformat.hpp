c193aa0a2eb3cebc519eb97977c9318416d787ec
Godet Léandre <leandre.godet@epitech.eu>
2025-12-09 18:44:33 +0100

    add refactor of io

--- include/io/epgformat.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <QImage>
-#include <QString>
-
-namespace EpgFormat
-{
-bool save(const QString& fileName, const QImage& image);
-bool load(const QString& fileName, QImage& outImage);
-}  // namespace EpgFormat
